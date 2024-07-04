51818a2183c00c2161e4ae82689e1cf071f2bc11
Zamyr Guevara <blinkx2003@gmail.com>
2024-07-03 19:30:38 -0500

    Migraciones corregidas

--- database/migrations/2024_07_03_190843_add_two_factor_columns_to_users_table.php
+++ database/migrations/2024_07_03_190843_add_two_factor_columns_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->timestamp('two_factor_confirmed_at')->nullable();
         });
     }
 
@@ -21,8 +23,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        // Schema::table('users', function (Blueprint $table) {
+        //     $table->dropColumn('two_factor_secret');
+        //     $table->dropColumn('two_factor_recovery_codes');
+        //     $table->dropColumn('two_factor_confirmed_at');
+        // });
     }
 };
